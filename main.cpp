6a13c93c8616ffaa747af1111e8c7f9eba1b3617
Vlad Zybkin <zybkin.vyu@edu.spbstu.ru>
2019-03-06 09:31:31 +0300

    Add files with prototyp of QueueMyVersion class

---
+++ main.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+#include "queue.hpp"
+
+int main()
+{
+
+  return 0;
+}
---
+++ queue.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "queue.hpp"
+
+template <typename T> QueueMyVersion<T>(T var)
+  : first_(new ElementOfQueue<Tp>(var))
+{
+
+}
