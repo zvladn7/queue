f73397a312755355e62d7249532f5761eca5ec90
Vlad Zybkin <zybkin.vyu@edu.spbstu.ru>
2019-03-07 00:14:01 +0300

    Description of prototypes

--- queue.cpp
+++ queue.cpp
@@ -1,8 +1,96 @@
 #include <iostream>
 #include "queue.hpp"
 
-template <typename T> QueueMyVersion<T>(T var)
-  : first_(new ElementOfQueue<Tp>(var))
-{
 
+//-----------------Usual constructor-------------------------
+template <typename T> QueueMyVersion<T>::QueueMyVersion(T var)
+	: first_(new ElementOfQueue<T>(var))
+{
+	last_ = first_;
+	size_ = 1;
+}
+
+//-----------------Constructor of copy one object into another-------------------------
+template <typename T> QueueMyVersion<T>::QueueMyVersion(QueueMyVersion const & copy)
+	: size_(copy.size_)
+{
+	ElementOfQueue<T> * time = copy.first_;
+	while (time != 0)
+	{
+		this->push(time->variable_);
+		time = time->prev_;
+	}
+}
+
+//-----------------destructor-------------------------
+template <typename T> QueueMyVersion<T>::~QueueMyVersion()
+{
+	first_ = 0;
+	ElementOfQueue<T> * time = last_;
+	while (last_ != 0)
+	{
+		last_ = last_->next_;
+		delete time;
+	}
+}
+
+//-----------------Method SWAP-------------------------
+template <typename T> void QueueMyVersion<T>::swap(QueueMyVersion & copy)
+{
+	std::swap(size_,copy.size_);
+	std::swap(first_,copy.fitst_);
+	std::swap(last_,copy.last_);
+}
+
+//-----------------OPERATOR =-------------------------
+template <typename T> QueueMyVersion<T> & QueueMyVersion<T>::operator=(QueueMyVersion const & copy)
+{
+	if (this != &copy)
+	{
+		QueueMyVersion(copy).swap(*this);
+	}
+	return *this;
+}
+
+//-----------------METHOD PUSH-------------------------
+template <typename T> void QueueMyVersion<T>::push(T var)
+{
+	if (size_ != 0)
+	{
+		last_->next_ = new ElementOfQueue<T>(var);
+		last_->next_->prev_ = last_;
+		last_ = last_->next_;
+	}
+	else
+	{
+		first_ = new ElementOfQueue<T>(var);
+		first_->next_ = 0;
+		first_->prev_ = 0;
+		last_ = first_;
+	}
+	size_++;
+}
+
+//---------------------METHOD FRONT----------------------------
+template <typename T> ElementOfQueue<T> * QueueMyVersion<T>::front()
+{
+	return first_;
+}
+
+//---------------------METHOD BACK----------------------------
+template <typename T> ElementOfQueue<T> * QueueMyVersion<T>::back()
+{
+	return last_;
+}
+
+//---------------------METHOD EMPTY----------------------------
+template <typename T> bool QueueMyVersion<T>::empty() const
+{
+	return (size_ == 0);
+}
+
+//---------------------METHOD SIZE----------------------------
+template <typename T> int QueueMyVersion<T>::size() const
+{
+	return size_;
 }
--- queue.hpp
+++ queue.hpp
@@ -2,38 +2,38 @@
 #define QUEUE_H
 #include <iostream>
 
-template <typename D> struct ElementOfQueue {
-  ElementOfQueue(D var)
-    : variable_(var)
-  {
-      next_ = 0;
-      prev_ = 0;
-  }
-  D variable_;
-  ElementOfQueue<D> * next_;
-  ElementOfQueue<D> * prev_;
+template <typename D = std::allocator<int> > struct ElementOfQueue {
+	ElementOfQueue(D var)
+		: variable_(var)
+	{
+		next_ = 0;
+		prev_ = 0;
+	}
+	D variable_;
+	ElementOfQueue<D> * next_;
+	ElementOfQueue<D> * prev_;
 };
 
-template <typename T> //= std::allocator<int> >
+template <typename T = std::allocator<int> >
 class QueueMyVersion {
 public:
-  QueueMyVersion(T var);                      // constructors
-  QueueMyVersion(QueueMyVersion const & copy);
-  virtual ~QueueMyVersion();             // destructor
-  T front();                             // get access for first element
-  T back();                              // get access for last added element
-  bool empty();                          // check that quece empty or not
-  int size();                            // return size of QUECE_H
-  void push();                           // push new element into the end of quece
-  void pop();                            // delete the first element in the quece
-  void swap(QueueMyVersion & copy);      // this method is swapping two object
-  QueueMyVersion &operator= (QueueMyVersion const & copy);
+QueueMyVersion(T var);                            // constructors
+QueueMyVersion(QueueMyVersion const & copy);
+virtual ~QueueMyVersion();               // destructor
+ElementOfQueue<T> * front();                               // get access for first element
+ElementOfQueue<T> * back();                                // get access for last added element
+bool empty() const;                            // check that quece empty or not
+int size() const;                              // return size of QUECE_H
+void push(T var);                             // push new element into the end of quece
+void pop();                              // delete the first element in the quece
+void swap(QueueMyVersion & copy);        // this method is swapping two object
+QueueMyVersion &operator= (QueueMyVersion const & copy);
 
 
 private:                                 // field which we can change only in this class
-  ElementOfQueue<T> * first_;
-  ElementOfQueue<T> * last_;
-  int size_;
+ElementOfQueue<T> * first_;
+ElementOfQueue<T> * last_;
+int size_;
 };
 
 #endif /* QUEUE_H */
